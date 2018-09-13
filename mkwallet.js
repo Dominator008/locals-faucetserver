d82414dc57b4b2208c7f342d127366054b584c1a
Michael Zhou <zhoumotongxue008@gmail.com>
2018-09-12 21:55:15 -0700

    Update mkwallet.js to work with eth-lightwallet 3

--- mkwallet.js
+++ mkwallet.js
@@ -6,9 +6,18 @@ if (!process.argv[2]) {
 	process.exit();
 }
 
-var secretSeed = lightwallet.keystore.generateRandomSeed();
-lightwallet.keystore.deriveKeyFromPassword(process.argv[2], (err, pwDerivedKey) => {
-	var keystore = new lightwallet.keystore(secretSeed, pwDerivedKey);
-	keystore.generateNewAddress(pwDerivedKey, 1);
-	console.log(keystore.serialize());
+var password = process.argv[2];
+
+lightwallet.keystore.createVault({
+  seedPhrase: 'question eager diagram hill fall marriage hub nuclear disease manage bread awkward',
+  password: password,
+  hdPathString: 'm/44\'/60\'/0\'/0'
+}, function (err, ks) {
+  ks.keyFromPassword(password, function(err, pwDerivedKey) {
+    if (err) {
+      throw err;
+    }
+    ks.generateNewAddress(pwDerivedKey, 1);
+    console.log(ks.serialize());
+  });
 });
